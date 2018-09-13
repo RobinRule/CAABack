5c69386522380fb7d7f05063b1c6c7a529c573bd
RobinRule <fengzhiyu17@msn.cn>
2018-09-13 00:08:17 -0400

    Backup

---
+++ src/caa/business/TransactionBusiness.py
@@ -0,0 +1,10 @@
+from db.Transaction import Transaction
+import logging
+
+logger = logging.getLogger(__name__)
+
+class TransactionBusiness(object):
+	"""docstring for TransactionBusiness"""
+	# @classmethod
+	# def get():
+	# 	pass
--- src/caa/db/Transaction.py
+++ src/caa/db/Transaction.py
@@ -1,10 +1,11 @@
 
 from .DBManager import DBManager
+from .Case import Case
 from datetime import datetime
 from dateutil.parser import parse as timeParse
 from boto3.dynamodb.conditions import Key, Attr
 import logging
-
+import uuid
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +13,8 @@ class Transaction(object):
     """docstring for Transaction"""
     FIELD_LIST = [
         'transactionId',
-        'caseIds'
+        'caseIds',
+        'transactionWindowSize'
     ]
     
     def __getattr__(self, key):
@@ -27,4 +29,24 @@ class Transaction(object):
             for key, val in jsonObj.iteritems():
                 if key in Transaction.FIELD_LIST:
                     self.attrMap[key] = jsonObj[key]
-        
+    @classmethod
+    def creatNewTransactionId(cls):
+        return "{}-{}".format( uuid.uuid4(), datetime.now().isoformat())
+
+    @classmethod
+    def createTransaction(cls, winSize, caseIds):
+
+        transactionId = cls.creatNewTransactionId()
+        logger.info("Transaction id : {} dispatched.".format(transactionId))
+
+        transactionTable = DBManager.table("Transactions")
+        transactionTable.put_item(
+            Item = {
+                'transactionId' : transactionId,
+                'caseIDs' : caseIds,
+                'transactionWindowSize' : winSize
+            }
+        )
+        logger.info("Added transaction: {} to database".format(transactionId))
+
+        return transactionId
